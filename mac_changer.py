a6cc57c55fa8d412ccd87275bc63b7922df619a9
Josh Phillips <josh@joshphillipssr.com>
2024-08-28 19:55:10 -0500

    Added variables and user input

--- mac_changer.py
+++ mac_changer.py
@@ -2,11 +2,11 @@
 
 import subprocess
 
-interface = "wlan0"
-new_mac = "00:11:22:33:44:55"
+interface = input("interface (e.g. eth0) > ")
+new_mac = input("mac address (e.g. 00:11:22:33:44:44) > ")
 
-print("[+] Changing MAC Address for " + interface + " to " + new_mac)
+print("[+] Changing MAC address for " + interface + " to " + new_mac)
 
-subprocess.call("ifconfig " + interface " down", shell=True)
-subprocess.call("ifconfig " + interface " hw ether " + new_mac, shell=True)
-subprocess.call("ifconfig " + interface " up", shell=True)
+subprocess.call("ifconfig " + interface + " down", shell=True)
+subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
+subprocess.call("ifconfig " + interface + " up", shell=True)
