746acd6ccbe3a50ac813b8a6dce0e042e4fc2ab5
Josh Phillips <josh@joshphillipssr.com>
2024-08-28 20:05:39 -0500

    Cleaned up subprocess to add list elements for security

--- mac_changer.py
+++ mac_changer.py
@@ -7,6 +7,6 @@ new_mac = input("mac address (e.g. 00:11:22:33:44:44) > ")
 
 print("[+] Changing MAC address for " + interface + " to " + new_mac)
 
-subprocess.call("ifconfig " + interface + " down", shell=True)
-subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
-subprocess.call("ifconfig " + interface + " up", shell=True)
+subprocess.call(["ifconfig", interface, "down"])
+subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+subprocess.call(["ifconfig", interface, "up"])
